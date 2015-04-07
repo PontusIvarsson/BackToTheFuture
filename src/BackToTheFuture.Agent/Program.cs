47335622fec888c5ef34852fdb5158c92b694b7c
Pontus Ivarsson <pontus_ivarsson@hotmail.com>
2015-04-07 18:31:09 +0200

    Fix last messup
    Fix last messup

--- src/BackToTheFuture.Agent/Program.cs
+++ src/BackToTheFuture.Agent/Program.cs
@@ -4,31 +4,18 @@ using BackToTheFuture.Core;
 using System.Diagnostics;
 using System.IO;
 using BackToTheFuture.Hosting.StandAloneHttpServer;
-<<<<<<< HEAD
-using BackToTheFuture.Core.SiteTargets;
-using Microsoft.Owin;
-using Microsoft.Owin.Hosting;
-using BackToTheFuture.Hosting;
-
-=======
 using Microsoft.Owin.Hosting;
 using Owin;
 using BackToTheFuture.Core.SiteTargets;
 using BackToTheFuture.Hosting.OwinComponents;
->>>>>>> origin/master
+
 namespace BackToTheFuture.Agent
 {
     //netsh http add urlacl url=http://+:80/ user=Everyone listen=yes
     //netsh http delete urlacl url=http://+:80/
     class Program
     {
-<<<<<<< HEAD
-        static SimpleStandAloneHttpServer myServer;
-        static int portNumber = 801;
-=======
         public static InMemorySiteTarget Site { get; set; }
->>>>>>> origin/master
-
         static void Main(string[] args)
         {
             var source = new DirectorySiteSource(new DirectoryInfo(@"c:\site"));
@@ -56,34 +43,6 @@ namespace BackToTheFuture.Agent
         }
     }
 
-<<<<<<< HEAD
-            //StartAsOwin(site);
-            StartAsSimpleStandAloneHttpServer(site);
-
-
-            Process.Start(string.Format("http://localhost:{0}", portNumber));
-            Console.ReadKey();
-        }
-
-        public static void StartAsOwin(ISiteTarget target)
-        {
-            string uri = "http://localhost:" + portNumber;
-            using (WebApp.Start<Startup>(uri))
-            {
-                Console.ReadKey();
-            }
-        }
-
-        public static void StartAsSimpleStandAloneHttpServer(ISiteTarget target)
-        {
-            myServer = new SimpleStandAloneHttpServer(@"D:\site", portNumber, target);
-        }
-
-        public static void TareDown()
-        {
-            if (myServer != null)
-                myServer.Stop();
-=======
     
     public class Startup
     {
@@ -91,7 +50,6 @@ namespace BackToTheFuture.Agent
         {
             var site = Program.Site;
             app.UseBackToTheFutureSite(site);
->>>>>>> origin/master
         }
     }
 }
